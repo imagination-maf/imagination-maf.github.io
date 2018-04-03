00db2b6f94d03619e32aa1686c2e1f5d52e5feab
dan.diggins <contact@dandiggins.co.uk>
2018-04-03 19:27:35 +0400

    updated host mode switching to use alternate websocket

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,16 @@
 <template>
 <main>
-    <host-mode></host-mode>
     <router-view></router-view>
 </main>
 </template>
 
 <script>
 import AppHeader from './components/header.vue';
-import HostMode from './components/hostMode.vue';
 
 export default {
 	name: 'App',
 	components: {
-		appHeader: AppHeader,
-		hostMode: HostMode
+		appHeader: AppHeader
 	}
 };
 </script>
