39988085e5aa33770adf51f4a3894fa4c25239b0
Lawrence Burden <lawrence.burden@imagination.com>
2018-04-04 03:24:42 +0400

    added initial gallery

--- src/components/propertyInfo/propertyInfoGallery.vue
+++ src/components/propertyInfo/propertyInfoGallery.vue
@@ -1,14 +1,31 @@
 <script>
 import Vue from 'vue';
+import config from '../../data/config.js';
 
 export default Vue.component('property-info-gallery', {
     props: ['property'],
     data() {
         return {
-            images: this.property.images.split(';')
+            slide: 0
+        }
+    },
+    computed: {
+        images: function() {
+            let images = this.property.images.split(';');
+            if(images.length > 0) {
+                let url = config.galleryUrl + this.$route.query.community + '/gallery/';
+
+                // due to inconsisency in paths having extension, remove on all and re-add
+                return images.map( (img) => url + img.replace('.jpg', '') + '.jpg' );
+            } else {
+                return null;
+            }
         }
     },
     methods: {
+        changeSlide: function(dir) {
+            this.slide = ((this.slide + dir) + this.images.length) % this.images.length;
+        },
         backToSummary: function() {
             this.$emit('changeView', 'summary');
         }
@@ -19,11 +36,30 @@ export default Vue.component('property-info-gallery', {
 <template>
     <div class="info-gallery">
         <h3 class="info-title">Photo Gallery</h3>
+        <div class="info-gallery-container">
+            <img v-for="(image, index) in images" v-if="slide === index" style="width: 100%;" :src="image" />
+            <button class="arrow left" type="button" @click="changeSlide(-1)"><</button>
+            <button class="arrow right" type="button" @click="changeSlide(1)">></button>
+        </div>
         <button class="info-button" type="button" @click="backToSummary()">Go Back to Summary</button>
     </div>
 </template>
 
 <style lang="scss" scoped>
+
+.info-content {
+    padding: 10rem 3rem 10rem 6rem !important;
+}
+
+.info-gallery-container {
+    width: 100%;
+    max-height: 60vh;
+    .gallery-image {
+        width: 100%;
+        object-fit: contain;
+        object-position: center;
+    }
+}
 .info-gallery {
     .info-title {
         color: #626263;
@@ -33,6 +69,22 @@ export default Vue.component('property-info-gallery', {
         letter-spacing: 0.08rem;
         font-weight: 200;
         font-size: 1.5rem;
+    }
+
+    .arrow {
+        background-color: #8a1337;
+        color: #ffffff;
+        width: 3rem;
+        height: 3rem;
+        position: absolute;
+        top: calc(50% - 1.5rem);
+        border-radius: 100%;
+        &.left {
+            left: 2rem;
+        }
+        &.right {
+            right: 2rem;
+        }
     }
 
     .info-button {
--- src/data/config.js
+++ src/data/config.js
@@ -4,6 +4,7 @@ export default {
         'staging': 'https://staging.imagination-maf.com/api/',
         'live': ''
     },
+    'galleryUrl': 'https://s3-eu-west-1.amazonaws.com/maf-gallery-images/',
     'masterplan-inactive': ['app_x5F_Lebanon-region--parent', 'app_x5F_Lebanon--parent', 'app_x5F_Lebanon-road--parent', 'app_x5F_Oman--parent', 'app_x5F_Muscat--parent', 'app_x5F_Muscat-road--parent', 'app_x5F_Dubai--parent', 'app_x5F_Dubai-road--parent'],
     'mappings': {
         'app_x5F_Sharjah-road--parent': 'alzahia'
