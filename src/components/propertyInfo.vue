881c01d154cb04cf94912b98989f8b370de9023c
Shaik Ahamed Rilwan <shaikrilwan@gmail.com>
2018-10-18 04:39:24 +0400

    added new pinch zoom for test

--- src/components/propertyInfo.vue
+++ src/components/propertyInfo.vue
@@ -30,27 +30,29 @@ export default Vue.component('property-info', {
     },
         mounted() {
 
-            var $panzoom = $('#info-zoom-container').panzoom({
-                minScale: 1,
-                contain: 'invert'
-            });
+            $('#info-zoom-container').pinchzoomer();
+
+            // var $panzoom = $('#info-zoom-container').panzoom({
+            //     minScale: 1,
+            //     contain: 'invert'
+            // });
 
             $('.info-cta-item').on('mousedown touchstart', function( e ) {
               e.stopImmediatePropagation();
             });
             
-            $panzoom.parent().on('mousewheel.focal', ( e ) => {
-                e.preventDefault();
-                var delta = e.delta || e.originalEvent.wheelDelta;
-                var zoomOut = delta ? delta < 0 : e.originalEvent.deltaY > 0;
-                $panzoom.panzoom('zoom', zoomOut, {
-                    increment: 0.1,
-                    animate: false,
-                    panOnlyWhenZoomed: true,
-                    minScale: 1,
-                    focal:e
-                });
-            });
+            // $panzoom.parent().on('mousewheel.focal', ( e ) => {
+            //     e.preventDefault();
+            //     var delta = e.delta || e.originalEvent.wheelDelta;
+            //     var zoomOut = delta ? delta < 0 : e.originalEvent.deltaY > 0;
+            //     $panzoom.panzoom('zoom', zoomOut, {
+            //         increment: 0.1,
+            //         animate: false,
+            //         panOnlyWhenZoomed: true,
+            //         minScale: 1,
+            //         focal:e
+            //     });
+            // });
     
         }
 });
